bf31954a73e3b0a4339fc2b856b4514f15dc1f9a
DiegoVilte <viltede@hotmail.com>
2021-06-22 06:39:21 -0300

    ServicePlan
    hay q agregar el deletPlan

--- src/app/models/plan.ts
+++ src/app/models/plan.ts
@@ -1,9 +1,10 @@
 export class Plan {
+    _id: string="";
     tipo: String="";
- fechaInicioPlan: Date=new Date;
- fechaFinDelPlan: Date=new Date;
- nivel: String="";
- objetivo: String="";
+    fechaInicioPlan: Date=new Date;
+    fechaFinDelPlan: Date=new Date;
+    nivel: String="";
+    objetivo: String="";
  //registroDieta: RegistroDieta;
  //pago: Cuota;
  //rutina: Rutina;
--- src/app/service/plan.service.ts
+++ src/app/service/plan.service.ts
@@ -1,9 +1,67 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Plan } from '../models/plan';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanService {
+  urlBase:string=""
+  constructor(private _http:HttpClient) {
+    this.urlBase="http://localhost:3000/api/";
+   }
 
-  constructor() { }
+   getAllPlanes():Observable<any>{
+    let option = {
+      headers: new HttpHeaders({
+
+      }),
+      params: new HttpParams({
+
+      })
+    }
+    return this._http.get(this.urlBase+"plan", option);
+   }
+   getPlan(id:string):Observable<any>{
+    let option = {
+      headers: new HttpHeaders({
+
+      }),
+      params: new HttpParams({
+
+      })
+    }
+    return this._http.get(this.urlBase+"plan/"+id, option);
+
+   }
+   postCrearPlan(plan:Plan){
+    const httpOptions= {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      }),
+      params: new HttpParams({   
+      })
+    }
+    let body=JSON.stringify(plan);
+    return this._http.post(this.urlBase+"plan/",body,httpOptions);
+   }
+   putEditarPlan(plan:Plan):Observable<any>{
+    const httpOption = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      }),
+      params: new HttpParams({
+
+      })
+    }
+
+    let body = JSON.stringify(plan);
+    return this._http.put(this.urlBase+"plan/"+plan._id, body, httpOption);
+   }
+
+   deletePlan(id:string){
+     
+   }
+
 }
