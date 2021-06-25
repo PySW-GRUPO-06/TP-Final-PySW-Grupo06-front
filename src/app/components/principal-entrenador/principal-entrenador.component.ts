d50dbc0085c25c00625ebb137d4df14d4ba7633e
jorgenick <jtolababsd@gmail.com>
2021-06-24 21:12:05 -0300

    Conexion de paginas: AgregarNuevoAlumno, agregarCuota, Asistencia , AgregarGenerarRutina 	modified:   src/app/components/principal-entrenador/principal-entrenador.component.html 	modified:   src/app/components/principal-entrenador/principal-entrenador.component.ts

--- src/app/components/principal-entrenador/principal-entrenador.component.ts
+++ src/app/components/principal-entrenador/principal-entrenador.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-principal-entrenador',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalEntrenadorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:Router,
+    private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
+  irAInscribirNuevoAlumno():void{
+    this.route.navigate(['inscribirNuevoAlumno']);
+ }
+
+ irARegistrarPagoCuota():void{
+  this.route.navigate(['registrarCuotas']);
+}
+
+irARegistrarAsistencia():void{
+  this.route.navigate(['registrarAsistencia']);
+}
+
+irAGenerarModifRutinas():void{
+  this.route.navigate(['administrarRutinas']);
+}
 }
