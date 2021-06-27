8e58f3e63fa38523deab54acdf000fd0b06f8614
jorgenick <jtolababsd@gmail.com>
2021-06-27 20:13:25 -0300

    Se agregan nuevas opciones en Pag Principal 	modified:   src/app/components/principal/principal.component.css 	modified:   src/app/components/principal/principal.component.html 	modified:   src/app/components/principal/principal.component.ts 	new file:   src/assets/gym/imagen15.jpg 	new file:   src/assets/gym/promo1.jpg 	new file:   src/assets/gym/promo2.jpg 	new file:   src/assets/gym/promo3.jpg

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -8,6 +8,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class PrincipalComponent implements OnInit {
 
+  pest1:boolean=true;
+  pest2:boolean=false;
+  pest3:boolean=false;
+  pest4:boolean=false;
+
   constructor(private route:Router,
     private activatedRoute:ActivatedRoute) { }
   
@@ -18,4 +23,25 @@ export class PrincipalComponent implements OnInit {
   registrarse():void{
      this.route.navigate(['login']);
   }
+
+  variacionPest1(){
+    this.pest1=true;
+    this.pest2=false;
+    this.pest3=false;
+
+      }
+
+  variacionPest2(){
+    this.pest1=false;
+    this.pest2=true;
+    this.pest3=false;
+
+    }
+
+    variacionPest3(){
+      this.pest1=false;
+      this.pest2=false;
+      this.pest3=true;
+        }
+
 }
